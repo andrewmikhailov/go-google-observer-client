9a8d7e7bf97ee0a40b48cbc14fcb2f77f78e419e
Andrew Mikhailov <andrew@vitche.com>
2017-09-08 17:14:41 +0300

    1. Implemented loading data from the news JSON API.

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+type Event struct {
+	Hash string `json:"hash"`
+	Text string `json:"text"`
+	Uri  string `json:"uri"`
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+
+	url := "http://google-observer-1.herokuapp.com/api/event/list?kernelIdentifier=593a842d7c52901100c8815c"
+	response, error := http.Get(url)
+	if error != nil {
+		panic(error.Error())
+	}
+
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		panic(error.Error())
+	}
+
+	var items [] Event
+	json.Unmarshal(body, &items)
+
+	for i := 1; i < len(items); i++ {
+
+		var item = items[i]
+		fmt.Printf("News event parsed: %s %s %s\n", item.Hash, item.Uri, item.Text)
+	}
 }
