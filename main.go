3fb68410ceb17da78cbabf4815e778ab24b62f52
Andrew Mikhailov <andrew@vitche.com>
2017-09-13 17:30:13 +0300

    1. Asynchronous loading for feed data.

--- main.go
+++ main.go
@@ -5,11 +5,26 @@ import (
 	"vitche.com/google-observer-client"
 )
 
+type EventFeed struct {
+	key   string
+	value []google_observer_client.Event
+}
+
 func main() {
-	uri := "http://google-observer-1.herokuapp.com/api/event/list?kernelIdentifier=593a842d7c52901100c8815c"
-	items := google_observer_client.NewDataSource(uri).Load()
-	for i := 1; i < len(items); i++ {
-		var item = items[i]
-		fmt.Printf("News event parsed: %s %s %s\n", item.Hash, item.Uri, item.Text)
+
+	feeds := make(chan EventFeed)
+
+	go func() {
+		uri := "http://google-observer-1.herokuapp.com/api/event/list?kernelIdentifier=593a842d7c52901100c8815c"
+		items := google_observer_client.NewDataSource(uri).Load()
+		feeds <- EventFeed{key: uri, value: items}
+	}()
+
+	select {
+	case eventFeed := <-feeds:
+		for i := 1; i < len(eventFeed.value); i++ {
+			var item = eventFeed.value[i]
+			fmt.Printf("News event parsed: %s %s %s\n", item.Hash, item.Uri, item.Text)
+		}
 	}
 }
